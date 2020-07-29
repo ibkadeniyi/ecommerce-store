197a48bce4ea8d0f05032b66a2acee97146697da
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-07-29 01:15:55 +0100

    updated menu item component and added routes property

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => ( 
+  <div>
+    <h1>HATS PAGES </h1>
+  </div>
+)
+
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
+    </div>
   );
 }
 
