ef33fd4bcf4e54909a39ef42123da9705c1b6e52
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-08-06 05:26:34 +0100

    modified cart dropdown to reflect when message when cart is empty and started building checkout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import SignInSignOut from './pages/sign-in-and-sign-out-page/sign-in-and-sign-out.component';
 import Header from './components/header/header.component.jsx';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action'; 
+import {selectCurrentUser} from './redux/user/user.selector';
 
 class App extends React.Component {
 
@@ -49,14 +53,19 @@ render() {
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
         <Route path='/sign-in' component={SignInSignOut} />
+        <Route exact path='/checkout' component={CheckoutPage} />
       </Switch>
     </div >
   )
 }
 }
 
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+});
+
 const mapDispatchToProp = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect (null, mapDispatchToProp) (App);
+export default connect (mapStateToProps, mapDispatchToProp) (App);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {createStructuredSelector} from 'reselect'
 
 import { toggleCartHidden } from '../../redux/cart/cart.action';
 import {selectCartItemsCount} from '../../redux/cart/cart.selector';
@@ -16,8 +17,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 );
 
 
-const mapStateToProps = (state) => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 })
 
 const mapDispatchToProps = dispatch => ({
