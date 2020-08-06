36babc1d9378302dfbff5f3fe3cc402489a45d7b
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-08-06 01:03:07 +0100

    updated code with reselect libary then replaced props with reselct props to improve performance

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import {selectCartItemsCount} from '../../redux/cart/cart.selector';
 
 import './cart-icon.styles.scss';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div onClick={toggleCartHidden} className='cart-icon'>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'> 0 </span>
+        <span className='item-count'> {itemCount} </span>
     </div>
 );
+
+
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
