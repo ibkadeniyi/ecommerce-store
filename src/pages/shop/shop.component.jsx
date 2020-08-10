b7c7d5f248b100917a3d2c3f72ac9cc1a767db98
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-08-10 03:33:18 +0100

    created nested components for shop page to render each category in own component and refractored object to reflect data normalization

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { Route } from 'react-router-dom';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import CollectionPage from '../collection/collection.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({match}) => (
     <div className='shop-page'>
-        {
-            collections.map(({ id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
-)
-
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
+);
 
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
